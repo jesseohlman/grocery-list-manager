7456932c8664fae97ef32f121fd98e89d73f2d7d
jesseohlman <44782018+jesseohlman@users.noreply.github.com>
2019-07-09 18:07:31 -0400

    working on production

--- server.js
+++ server.js
@@ -21,6 +21,17 @@ server.listen(port);
    return false;
  }
 
+ if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
  server.on("listening", () => {
   console.log(`server is listening for requests on port ${server.address().port}`);
 });
