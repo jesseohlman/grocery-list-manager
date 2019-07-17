909dad113a1d5ed12151098f9b3cdeed2e52f7a2
jesseohlman <44782018+jesseohlman@users.noreply.github.com>
2019-07-17 14:41:22 -0400

    fixing item complete

--- client/src/components/item.js
+++ client/src/components/item.js
@@ -6,7 +6,6 @@ class Item extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        item: {},
         isCompleted: false
     };
 
@@ -15,7 +14,7 @@ class Item extends Component {
   }
 
   componentDidMount(){
-    this.setState({item: this.props.item, isCompleted: this.props.item.isAquired});
+    this.setState({isCompleted: this.props.item.isAquired});
   }
   
   handleComplete(e, itemId){
@@ -28,7 +27,7 @@ class Item extends Component {
     .then((res) => {
       //changes completed to true if false and vise versa
       var change = this.state.isCompleted ? false : true;
-      this.setState({item: this.state.item, isCompleted: change})
+      this.setState({isCompleted: change})
     })
     .catch((err) => {
       console.log(err);
@@ -40,18 +39,18 @@ class Item extends Component {
     return (
       <div>
         <div className="name-display">
-          <strong>Name:</strong> {this.state.item.name}
+          <strong>Name:</strong> {this.props.item.name}
         </div>
         
         <small className="amount-display">
-          <strong>Amount:</strong> {this.state.item.count}
+          <strong>Amount:</strong> {this.props.item.count}
         </small>
 
         <div className="complete-box">
           <label for="complete">Item Aquired: </label>
           {this.state.isCompleted ? 
-          (<input type="checkbox" name="complete" onChange={(e) => this.handleComplete(e, this.state.item.id)} checked/>) : 
-          (<input type="checkbox" name="complete" onChange={(e) => this.handleComplete(e, this.state.item.id)}/>)} 
+          (<input type="checkbox" name="complete" onChange={(e) => this.handleComplete(e, this.props.item.id)} checked/>) : 
+          (<input type="checkbox" name="complete" onChange={(e) => this.handleComplete(e, this.props.item.id)}/>)} 
         </div>     
       </div>
     )
